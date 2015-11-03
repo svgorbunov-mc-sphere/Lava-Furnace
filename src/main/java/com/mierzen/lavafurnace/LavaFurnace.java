bccea38c70441a339e9b67115d424d001aafe61e
Mierzen <steptaljaard@gmail.com>
2015-11-03 15:57:51 +0200

    Add blank mod

---
+++ src/main/java/com/mierzen/lavafurnace/LavaFurnace.java
@@ -0,0 +1,37 @@
+package com.mierzen.lavafurnace;
+
+import com.mierzen.lavafurnace.proxies.CommonProxy;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
+public class LavaFurnace
+{
+    @Mod.Instance
+    public static LavaFurnace instance = new LavaFurnace();
+
+    @SidedProxy(clientSide = "com.mierzen.lavafurnace.proxies.ClientProxy", serverSide = "com.mierzen.lavafurnace.proxies.ServerProxy")
+    public static CommonProxy proxy;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent e)
+    {
+        proxy.preInit(e);
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent e)
+    {
+        proxy.init(e);
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent e)
+    {
+        proxy.postInit(e);
+    }
+}
---
+++ src/main/java/com/mierzen/lavafurnace/Reference.java
@@ -0,0 +1,8 @@
+package com.mierzen.lavafurnace;
+
+public class Reference
+{
+    public static final String MODID = "lavafurnace";
+    public static final String NAME = "Lava Furnace";
+    public static final String VERSION = "1.0";
+}
